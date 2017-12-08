3c59a73437f607a0f902f37290803ad90f7fda72
kurozakizz <squallcope@gmail.com>
2017-12-08 15:08:12 +0700

    Add setup and teardown method

--- LogAnalyzerTests.cs
+++ LogAnalyzerTests.cs
@@ -3,20 +3,26 @@ using Xunit;
 
 namespace dotnet_testing
 {
-    public class LogAnalyzerTests
+    public class LogAnalyzerTests: IDisposable
     {
+        private LogAnalyzer logAnalyzer = null;
+
+        public LogAnalyzerTests()
+        {
+            logAnalyzer = new LogAnalyzer();
+        }
+
+        public void Dispose()
+        {
+            logAnalyzer = null;
+        }
+
         [Theory]
         [InlineData("filename.png", false)]
         [InlineData("filename.log", true)]
         public void Test_IsValidLogFileName_PngExtension_ShouldReturn_False(string fileName, bool expected)
         {
-            // arrange
-            LogAnalyzer logAnalyzer = new LogAnalyzer();
-
-            // act
             bool actual = logAnalyzer.IsValidLogFileName(fileName);
-
-            // assert
             Assert.Equal(expected, actual);
         }
     }
