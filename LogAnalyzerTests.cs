0fbfe8776c053ffa1b299238cc4c110885283934
kurozakizz <squallcope@gmail.com>
2017-12-08 14:57:43 +0700

    Merge test case into one, using [Theory] instead of [Fact]

--- LogAnalyzerTests.cs
+++ LogAnalyzerTests.cs
@@ -5,27 +5,19 @@ namespace dotnet_testing
 {
     public class LogAnalyzerTests
     {
-        [Fact]
-        public void Test_IsValidLogFileName_PngExtension_ShouldReturn_False()
+        [Theory]
+        [InlineData("filename.png", false)]
+        [InlineData("filename.log", true)]
+        public void Test_IsValidLogFileName_PngExtension_ShouldReturn_False(string fileName, bool expected)
         {
             // arrange
             LogAnalyzer logAnalyzer = new LogAnalyzer();
 
             // act
-            bool actual = logAnalyzer.IsValidLogFileName("filename.png");
+            bool actual = logAnalyzer.IsValidLogFileName(fileName);
 
             // assert
-            Assert.False(actual);
-        }
-
-        [Fact]
-        public void Test_IsValidLogFileName_LogExtension_ShouldReturn_True()
-        {
-            LogAnalyzer logAnalyzer = new LogAnalyzer();
-
-            bool actual = logAnalyzer.IsValidLogFileName("filename.log");
-
-            Assert.True(actual);
+            Assert.Equal(expected, actual);
         }
     }
 }
